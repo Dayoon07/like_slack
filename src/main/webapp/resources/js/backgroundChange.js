2ed26d14184a3f2857bdc4b9831ada5cd5b7cbe8
Da yoon <gangd0642@gmail.com>
2024-11-26 18:32:06 +0900

    Update backgroundChange.js

--- src/main/webapp/resources/js/backgroundChange.js
+++ src/main/webapp/resources/js/backgroundChange.js
@@ -8,9 +8,6 @@ function blackAndWhite() {
 	if (!chkbg.checked) {
 		bg.classList.remove("bg-gray-900", "border-t", "border-b", "border-gray-700");
 		
-		chatHeader.classList.add("bg-blue-500");
-		chatHeader.classList.remove("bg-gray-900");
-		
 		mf.classList.add("border-gray-300", "bg-gray-100", "border-t");
 		mf.classList.remove("bg-gray-900");
 		
@@ -18,9 +15,6 @@ function blackAndWhite() {
 	} else {
 		bg.classList.add("bg-gray-900", "border-t", "border-b", "border-gray-700");
 		bg.classList.remove("bg-gray-100");
-		
-		chatHeader.classList.add("bg-gray-900");
-		chatHeader.classList.remove("bg-blue-500");
 		
 		mf.classList.add("bg-gray-900");
 		mf.classList.remove("bg-gray-100", "border-gray-300", "border-t");
